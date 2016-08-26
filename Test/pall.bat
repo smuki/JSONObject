f0df12f24e369046b776333769ec457d6450bc9c
sunriseyuen <sunriseyuen@gmail.com>
2016-08-26 16:56:44 +0800

    -

--- Test/pall.bat
+++ Test/pall.bat
@@ -1,4 +1,4 @@
 @echo off
-copy ..\dist\Volte.Data.JsonObject.dll Volte.Data.Json.dll
-copy ..\dist\Volte.Data.JsonObject.pdb Volte.Data.Json.pdb
+copy ..\dist\Volte.Data.Json.dll Volte.Data.Json.dll
+copy ..\dist\Volte.Data.Json.pdb Volte.Data.Json.pdb
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\csc test.cs /w:2 /debug+ /r:Volte.Data.Json.dll
