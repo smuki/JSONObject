cfe4c7d41616ddd52f186f9ea4ac961fb2aaaafc
sunrise <yjy@ekuaibao.com>
2017-06-25 22:15:05 +0800

    --

---
+++ Package.bat
@@ -0,0 +1,8 @@
+nuget install -ExcludeVersion -SolutionDirectory .
+
+mkdir %~dp0temp
+nuget pack %~dp0src\JSONObject.csproj -build -Symbols -Prop Configuration=Release -OutputDirectory %~dp0temp
+
+nuget.exe push %~dp0temp\JSONObject*.nupkg 123456 -Source http://localhost/nuget/api/v2/package
+del /q %~dp0temp\*
+rmdir %~dp0temp
