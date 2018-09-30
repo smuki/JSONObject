ba2faf0b916c0666971248466484eb5ac402c8e9
sunrise <sunriseyuen@qq.com>
2018-10-01 06:22:20 +0800

    build & test

--- Test/pall.bat
+++ Test/pall.bat
@@ -1,4 +1,8 @@
 @echo off
+cd ..\src
+nant
+cd ..\test
+
 copy ..\dist\Volte.Data.Json.dll Volte.Data.Json.dll
 copy ..\dist\Volte.Data.Json.pdb Volte.Data.Json.pdb
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\csc test.cs /w:2 /debug+ /r:Volte.Data.Json.dll,Newtonsoft.Json.dll,Volte.Utils.dll
